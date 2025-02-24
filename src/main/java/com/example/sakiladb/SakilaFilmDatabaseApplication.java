a02fc5cad9fd751dbe50b8e5cb2132095b33337b
Joseph <jbenson@softwareinstitute.com>
2025-02-24 14:24:20 +0000

    First Commit

---
+++ src/main/java/com/example/sakiladb/SakilaFilmDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sakiladb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaFilmDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaFilmDatabaseApplication.class, args);
+	}
+
+}
