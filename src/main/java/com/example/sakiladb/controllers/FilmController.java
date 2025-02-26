a5250963431a7864a3336b92ac94dba3ad181832
Joseph <jbenson@softwareinstitute.com>
2025-02-26 10:41:49 +0000

    Added Film Deleting

--- src/main/java/com/example/sakiladb/controllers/FilmController.java
+++ src/main/java/com/example/sakiladb/controllers/FilmController.java
@@ -7,6 +7,7 @@ import com.example.sakiladb.dto.response.FilmResponse;
 import com.example.sakiladb.dto.response.PartialFilmResponse;
 import com.example.sakiladb.entities.Actor;
 import com.example.sakiladb.entities.Film;
+import com.example.sakiladb.entities.Language;
 import com.example.sakiladb.repos.FilmRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -53,17 +54,19 @@ public class FilmController {
         final var newFilm = filmRepo.findById(savedFilm.getId()).orElseThrow(() -> new RuntimeException("Expected created film to exist!"));
         return FilmResponse.from(newFilm);
     }
-/*
-    @PatchMapping("/films")
-    public ActorResponse updateActor(@RequestBody ActorRequest data){
-        Film film = filmRepo.findById(data.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Actor Exists with that id"));
-        film.data
 
-        final var savedActor = actorRepo.save(actor);
-        final var newActor = actorRepo.findById(savedActor.getId()).orElseThrow(() -> new RuntimeException("Expected created actor to exist!"));
-        return ActorResponse.from(newActor);
+    @PatchMapping("/films")
+    public FilmResponse updateFilm(@RequestBody FilmRequest data){
+        Film film = filmRepo.findById(data.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Film Exists with that id"));
+        film.setTitle(data.getTitle());
+        film.setLanguageID((byte) 1);
+        film.setOriginalLanguageId((byte) 1);
+
+        final var savedFilm = filmRepo.save(film);
+        final var newFilm = filmRepo.findById(savedFilm.getId()).orElseThrow(() -> new RuntimeException("Expected created film to exist!"));
+        return FilmResponse.from(savedFilm);
     }
-*/
+
     @DeleteMapping("/films/{id}")
     public void deleteActor(@PathVariable Short id){
         Film film = filmRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Film Exists with that id"));
